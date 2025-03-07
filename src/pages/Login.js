a462edabb5c8891047311db69a9ade47e62e1484
Achyuta12345 <panda9191achyuta@gmail.com>
2025-03-07 18:06:03 +0530

    Initial Set up

---
+++ src/pages/Login.js
@@ -0,0 +1,63 @@
+import React from "react";
+import '@ant-design/v5-patch-for-react-19';
+import { Form, Input, message } from "antd";
+import "../styles/LoginStyle.css";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { showLoading,hideLoading } from "../redux/features/alertSlice";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  // Form handler
+  const onFinishHandler = async (values) => {
+    // console.log(values);
+    try {
+      dispatch(showLoading());
+      const res = await axios.post("/api/v1/user/login", values);
+      window.location.reload()
+      dispatch(hideLoading());
+
+      if (res.data.success) {
+        localStorage.setItem("token", res.data.token);
+        message.success("Login success");
+        navigate("/");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (err) {
+      dispatch(hideLoading());
+      console.log(err);
+      message.error("Something wrong");
+    }
+  };
+  return (
+    <>
+      <div className="form-container">
+        <Form
+          layout="vertical"
+          onFinish={onFinishHandler}
+          className="register-form"
+        >
+          <h1 className="text-center">User Login</h1>
+
+          <Form.Item label="Email" name="email">
+            <Input type="text" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
+          <button className="btn btn-primary" type="submit">
+            Login
+          </button>
+          <Link to="/register" className="m-2">
+            New User? Register
+          </Link>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/pages/Register.js
@@ -0,0 +1,58 @@
+import React from "react";
+import '@ant-design/v5-patch-for-react-19';
+import { Form,Input,message } from "antd";
+import '@ant-design/v5-patch-for-react-19';
+import "../styles/RegisterStyle.css";
+import {Link,useNavigate} from 'react-router-dom';
+import axios from 'axios';
+import { useDispatch } from "react-redux";
+import { hideLoading, showLoading } from "../redux/features/alertSlice";
+
+
+const Register = () => {
+const navigate = useNavigate();
+const dispatch = useDispatch();
+    // Form handler
+  const onFinishHandler = async (values) => {
+    // console.log(values);
+    try{
+      dispatch(showLoading());
+      const res = await axios.post('/api/v1/user/register',values);
+      dispatch(hideLoading());
+      if(res.data.success){
+        message.success("Registerd successfully");
+        navigate('/login');
+      }else{
+        message.error(res.data.message);
+      }
+    }catch(err){
+      dispatch(hideLoading());
+      console.log(err);
+      message.error('something went wrong')
+    }
+  };
+  return (
+    <>
+      <div className="form-container">
+        <Form layout="vertical" onFinish={onFinishHandler} className="register-form">
+          <h1 className="text-center">User Registeration</h1>
+          <Form.Item label="Name" name="name">
+            <Input type="text" required />
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input type="text" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
+          <button className="btn btn-primary" type="submit">
+            Register
+          </button>
+          <Link to='/login' className='m-2'>Already registered? Login</Link>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Register;
