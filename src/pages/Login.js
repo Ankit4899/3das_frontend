b5a11ecdba581c687920b9a6b92216572e0091ea
Ankit raj <ankitrajbarhiya51@gmail.com>
2025-07-17 11:41:24 +0530

    ui update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,69 @@
+// import React from "react";
+// import '@ant-design/v5-patch-for-react-19';
+// import { Form, Input, message } from "antd";
+// import "../styles/LoginStyle.css";
+// import { Link, useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import { useDispatch } from "react-redux";
+// import { showLoading,hideLoading } from "../redux/features/alertSlice";
+
+// const Login = () => {
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+//   const apiUrl = process.env.REACT_APP_API_URL;
+//   // Form handler
+//   const onFinishHandler = async (values) => {
+//     // console.log(values);
+//     try {
+//       dispatch(showLoading());
+//       const res = await axios.post(`${apiUrl}/api/v1/user/login`, values);
+//       window.location.reload()
+//       dispatch(hideLoading());
+
+//       if (res.data.success) {
+//         localStorage.setItem("token", res.data.token);
+//         message.success("Login success");
+//         navigate("/");
+//       } else {
+//         message.error(res.data.message);
+//       }
+//     } catch (err) {
+//       dispatch(hideLoading());
+//       console.log(err);
+//       message.error("Something wrong");
+//     }
+//   };
+//   console.log("login",apiUrl);
+//   return (
+//     <>
+//       <div className="form-container">
+//         <Form
+//           layout="vertical"
+//           onFinish={onFinishHandler}
+//           className="register-form"
+//         >
+//           <h1 className="text-center">User Login</h1>
+
+//           <Form.Item label="Email" name="email">
+//             <Input type="text" required />
+//           </Form.Item>
+//           <Form.Item label="Password" name="password">
+//             <Input type="password" required />
+//           </Form.Item>
+//           <button className="btn btn-primary" type="submit">
+//             Login
+//           </button>
+//           <Link to="/register" className="m-2">
+//             New User? Register
+//           </Link>
+//         </Form>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Login;
+
 import React from "react";
 import '@ant-design/v5-patch-for-react-19';
 import { Form, Input, message } from "antd";
@@ -10,13 +76,12 @@ import { showLoading,hideLoading } from "../redux/features/alertSlice";
 const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const apiUrl = process.env.REACT_APP_API_URL;
   // Form handler
   const onFinishHandler = async (values) => {
     // console.log(values);
     try {
       dispatch(showLoading());
-      const res = await axios.post(`${apiUrl}/api/v1/user/login`, values);
+      const res = await axios.post("/api/v1/user/login", values);
       window.location.reload()
       dispatch(hideLoading());
 
@@ -33,9 +98,14 @@ const Login = () => {
       message.error("Something wrong");
     }
   };
-  console.log("login",apiUrl);
   return (
-    <>
+    <div className="container">
+{/* Doctor Illustration */}
+{ <img
+src="/doc.jpg"  // Ensure this image is in your public folder
+alt="Doctor"
+className="doctor-img"
+/> }
       <div className="form-container">
         <Form
           layout="vertical"
@@ -58,7 +128,7 @@ const Login = () => {
           </Link>
         </Form>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,3 +1,64 @@
+// import React from "react";
+// import '@ant-design/v5-patch-for-react-19';
+// import { Form,Input,message } from "antd";
+// import '@ant-design/v5-patch-for-react-19';
+// import "../styles/RegisterStyle.css";
+// import {Link,useNavigate} from 'react-router-dom';
+// import axios from 'axios';
+// import { useDispatch } from "react-redux";
+// import { hideLoading, showLoading } from "../redux/features/alertSlice";
+
+
+// const Register = () => {
+// const navigate = useNavigate();
+// const dispatch = useDispatch();
+// const apiUrl = process.env.REACT_APP_API_URL;
+//     // Form handler
+//   const onFinishHandler = async (values) => {
+//     // console.log(values);
+//     try{
+//       dispatch(showLoading());
+//       const res = await axios.post(`${apiUrl}/api/v1/user/register`,values);
+//       dispatch(hideLoading());
+//       if(res.data.success){
+//         message.success("Registerd successfully");
+//         navigate('/login');
+//       }else{
+//         message.error(res.data.message);
+//       }
+//     }catch(err){
+//       dispatch(hideLoading());
+//       console.log(err);
+//       message.error('something went wrong')
+//     }
+//   };
+//   return (
+//     <>
+//       <div className="form-container">
+//         <Form layout="vertical" onFinish={onFinishHandler} className="register-form">
+//           <h1 className="text-center">User Registeration</h1>
+//           <Form.Item label="Name" name="name">
+//             <Input type="text" required />
+//           </Form.Item>
+//           <Form.Item label="Email" name="email">
+//             <Input type="text" required />
+//           </Form.Item>
+//           <Form.Item label="Password" name="password">
+//             <Input type="password" required />
+//           </Form.Item>
+//           <button className="btn btn-primary" type="submit">
+//             Register
+//           </button>
+//           <Link to='/login' className='m-2'>Already registered? Login</Link>
+//         </Form>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Register;
+
+
 import React from "react";
 import '@ant-design/v5-patch-for-react-19';
 import { Form,Input,message } from "antd";
@@ -12,13 +73,12 @@ import { hideLoading, showLoading } from "../redux/features/alertSlice";
 const Register = () => {
 const navigate = useNavigate();
 const dispatch = useDispatch();
-const apiUrl = process.env.REACT_APP_API_URL;
     // Form handler
   const onFinishHandler = async (values) => {
     // console.log(values);
     try{
       dispatch(showLoading());
-      const res = await axios.post(`${apiUrl}/api/v1/user/register`,values);
+      const res = await axios.post('/api/v1/user/register',values);
       dispatch(hideLoading());
       if(res.data.success){
         message.success("Registerd successfully");
@@ -33,7 +93,12 @@ const apiUrl = process.env.REACT_APP_API_URL;
     }
   };
   return (
-    <>
+    <div className="container">
+    { <img
+src="/das.jpg"  // Ensure this image is in your public folder
+alt="Doctor"
+className="doctor-img"
+/> }
       <div className="form-container">
         <Form layout="vertical" onFinish={onFinishHandler} className="register-form">
           <h1 className="text-center">User Registeration</h1>
@@ -52,7 +117,7 @@ const apiUrl = process.env.REACT_APP_API_URL;
           <Link to='/login' className='m-2'>Already registered? Login</Link>
         </Form>
       </div>
-    </>
+    </div>
   );
 };
 
